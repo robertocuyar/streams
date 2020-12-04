508060ead18fc2f3261928183a5b33bc3f896e74
Roberto <roberto.cuyar44@gmail.com>
2020-12-04 12:23:13 -0600

    Added Create Stream functionality linked to a Json-server api

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import App from './components/App';
 import reducers from './reducers';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers,
-    composeEnhancers(applyMiddleware()));
+    composeEnhancers(applyMiddleware(reduxThunk)));
+
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.querySelector('#root'));
